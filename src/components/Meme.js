7b284d1292e797248ca6b04e397db9c9b5b0bd54
Ekansh Saxena <ekansh.a.saxena@capgemini.com>
2022-02-14 19:32:44 +0200

    Add meme component

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,3 +1,34 @@
+import { useState } from "react";
+import memeData from "../memeData";
+
 export default function Meme(props) {
-  return <h2>Meme</h2>;
+  let [meme, setMeme] = useState({
+    id: "1",
+    topText: "shut up",
+    bottomText: "take my money",
+    image: "https://i.imgflip.com/1bij.jpg",
+  });
+
+  function handleOnClick() {
+    const aMemes = memeData.data.memes;
+    const randomIndex = Math.floor(Math.random() * aMemes.length);
+    const newImageUrl = aMemes[randomIndex].url;
+    setMeme((prevMeme) => ({
+      ...prevMeme,
+      image: newImageUrl,
+    }));
+  }
+
+  return (
+    <section>
+      <div className="form">
+        <input type="text" className="form--input" placeholder="Top text"></input>
+        <input type="text" className="form--input" placeholder="Bottom text"></input>
+      </div>
+      <button className="form--button" onClick={handleOnClick}>
+        Get a new meme image 🖼
+      </button>
+      <img src={meme.image} alt="meme" className="meme--image"></img>
+    </section>
+  );
 }
